597b2696a10de1789e214e9a749feb2ad393c8d0
neetukumari <93310377+neetulumari@users.noreply.github.com>
2021-12-23 22:59:12 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -1,18 +1,14 @@
-# calculator #
-first =int(input ("enter first number"))
-second =int(input ("enter first number"))
-operator =(input ("enter first operator(+,-,*,/,%) :"))
-if operator == "+":
-    print(first + second)
-elif operator == "-":
-    print(first - second)
-elif operator == "*":
-    print(first * second)
-elif operator == "/":
-    print(first / second)
-elif operator == "%":
-    print(first % second)
-else:
-    print("invalid number")
+# kirana store calculator#
+sum = 0
+while(True):
+    userInput = (input("enter the price  or press q to quit : /n "))
+    if(userInput!='q'):
+        sum = sum + int(userInput)
+        print(f"order total  so far : {sum}")
 
+    else:
+        print(f"your  total bill is {sum}.thanks for shoping us") 
 
+        break   
+    
+        
